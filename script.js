fe9e39a1826f76c2f5abc9ad08b68830a72bcfa6
Anthony <anthonyharisson30@gmail.com>
2022-06-15 17:11:49 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+for(let i = 1; i <= 10; i++){
+    console.log(i);
+  }
+  
+let num = 10;
+while(num >= 1){
+    console.log(num);
+    num--
+}
+  
+let conta = 1;
+while(conta <= 100 ){
+  console.log(conta)
+  conta += 2
+}
+  
+let verifica = 0
+while(verifica <= 100){
+  console.log(verifica)
+  verifica += 2
+}
